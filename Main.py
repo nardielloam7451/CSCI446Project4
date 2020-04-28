b90862d9056acfe53099009ffd2d1eed715da054
nardielloam <nardielloam@gmail.com>
2020-04-27 22:37:31 -0600

    adds example of printTrack to main

--- Main.py
+++ Main.py
@@ -3,8 +3,7 @@ import raceTrack
 def main():
     rTrack=raceTrack.RaceTrack()
     rTrack.createTrack('R-track.txt')
-    print(rTrack.getXSize())
-    print(rTrack.getYsize())
+    print(rTrack.printTrack())
 
 main()
     
