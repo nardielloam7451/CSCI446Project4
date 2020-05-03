bc93246283337e4e8a224c8e0a3c0428390fd9a4
Kyle Webster <kyle1234webster@gmail.com>
2020-05-03 15:41:20 -0600

    Added Graph Representation

--- QLearning.py
+++ QLearning.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import raceTrack
+import matplotlib.pyplot as plt
 
 class QLearning:
     def __init__(self, rTrack):
@@ -13,7 +14,9 @@ class QLearning:
         self.cost = 0
         self.startLocations = self.getStart()
         self.crossed = False
-        self.totalCost = 0
+        self.totalCost = [0]
+        self.averageReward = [0]
+
 
     def getStart(self):
         startLocations = []
@@ -97,35 +100,61 @@ class QLearning:
         if self.crossed:
             return 0
         else:
-            self.totalCost += 1
-            return -1
+            self.totalCost[-1] += 1
+            return 1
 
-    def selectAction(self, x, y):
-        if random.uniform(0, 1) < 0.2:
+    def selectAction(self, x, y, epsilon = .1):
+        if random.uniform(0, 1) < epsilon:
             return self.actions[random.choice(range(len(self.actions)))]
         else:
-            return self.actions[self.qTable[x][y].argmax()]
+            return self.actions[self.qTable[x][y].argmin()]
 
+    def getAverageReward(self):
+        playable = []
+        rewardList = []
+        for i in range(len(self.raceTrack)):
+            for j in range(len(self.raceTrack[i])):
+                if self.raceTrack[i][j] != '#' and self.raceTrack[i][j] != 'F':
+                    playable.append([i,j])
+        for i in range(len(playable)):
+            rewardList.append(self.qTable[playable[i][0]][playable[i][1]].mean())
+        return np.asarray(rewardList).mean()
+
+    def showGraphs(self, runs):
+        plt.figure()
+        plt.subplot(211)
+        plt.plot(list(range(runs)), self.averageReward[:-1])
+        plt.xlabel('Runs')
+        plt.ylabel('Average Cost in Given Run')
+        plt.subplot(212)
+        plt.plot(list(range(runs)), self.totalCost[:-1])
+        plt.xlabel('Runs')
+        plt.ylabel('Total Actions For Each Run')
+        plt.show()
     def qLearning(self, runs):
         for i in range(runs):
             location = random.choice(self.startLocations)
-            a = self.selectAction(location[0], location[1])
+            a = self.selectAction(location[0], location[1], .2)
             while not self.crossed:
                 newLocation = self.applyAction(location[0], location[1], a)
-                newA = self.selectAction(location[0], location[1])
+                newA = self.selectAction(location[0], location[1], 0.2)
                 reward = self.reward()
-                self.updateQ(a, location[0], location[1], 0.5, 0.8, newLocation[0], newLocation[1], newA, reward)
+                if reward:
+                    self.updateQ(a, location[0], location[1], 0.5, 0.95, newLocation[0], newLocation[1], newA, reward)
                 location = newLocation
                 a = newA
-            print(self.totalCost)
-            self.totalCost = 0
+            self.averageReward[-1] = self.getAverageReward()
+            self.averageReward.append(0)
+            self.totalCost.append(0)
             self.crossed = False
             self.velocity = (0,0)
-            self.track.printTrack()
             self.track.restartRace()
+        print(self.averageReward[:-1])
+        print(self.totalCost[:-1])
+        self.showGraphs(runs)
 
 
 qLearn = raceTrack.RaceTrack()
 qLearn.createTrack("L-track.txt")
 temp = QLearning(qLearn)
-temp.qLearning(15)
+temp.qLearning(10000)
