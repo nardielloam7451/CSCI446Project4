85e5fc6027b18fb2232abfb151346127c395ad53
nardielloam <nardielloam@gmail.com>
2020-05-03 18:24:44 -0600

    finishes bug fixing on Value Iteration

--- ValueIteration.py
+++ ValueIteration.py
@@ -1,19 +1,21 @@
 import numpy as np
-import random
 import raceTrack
+import random
+from copy import deepcopy
 
 class ValueIteration:
     def __init__(self, rTrack):
         self.track = rTrack
         self.raceTrack = self.track.getTrack()
-        self.actions[(-1,-1),(0,-1),(1,-1),(-1,0),(0,0),(1,0),(-1,1),(0,1),(1,1)]
-        self.qTable = np.zeros((rTrack.getXSize(),rTrack.getYsize(), len(self.actions)))
+        self.actions=[(-1,-1),(0,-1),(1,-1),(-1,0),(0,0),(1,0),(-1,1),(0,1),(1,1)]
+        self.minVelocity = -5
+        self.maxVelocity = 5
+        self.velocities = range(self.minVelocity, self.maxVelocity+1)
+        self.qTable = [[[[[random.Random() for _ in self.actions] for _ in self.velocities] for _ in (self.velocities)] for _ in line] for line in self.raceTrack]
         self.cost = 0
         self.startLocations = self.getStart()
         self.crossed = False
         self.totalCost = 0
-        self.minVelocity = -1
-        self.maxVelocity = 1
         self.discRate = 0.9 #The Discount rate, also known as gamma. 
         self.errorThres = 0.001 #The Error Threshold
         self.probAccelFail = 0.20 #The probability that the acceleration will fail
@@ -53,38 +55,9 @@ class ValueIteration:
         if newYVel < self.minVelocity: newYVel = self.minVelocity
         if newYVel > self.maxVelocity: newYVel = self.maxVelocity
 
-        return newYVel, newXVel
+        return newXVel, newYVel
 
-    def getNewPosition(self,xPos, yPos, xVel, yVel, punishment):
-        """
-        Get a new position using the old position and the velocity
-        Args:
-            xPos: The Old X position for the racer
-            yPos: The Old Y Position for the racer
-            xVel: The xVelocity of the racer
-            yVel: The y-Velocity of the racer
-            punishment: the crash penalty
-        Returns: 
-            The new X and Y coordinate and, but returns either the old position of 0,0 if racer crashed
-        """
-        newXPos= xPos+xVel
-        newYPos = yPos+yVel
-        check =self.track.getNewPosition(newXPos, newYPos)
-        if check =="Hit Wall":
-            if punishment == "Restart":
-                newXPos = self.startLocations[0][0]
-                newYPos = self.startLocations[0][1]
-                return newXPos, newYPos, "Restart"
-            elif punishment == 'Resume':
-                newXPos = xPos
-                newYPos = yPos
-                return newXPos, newYPos, "Hit Wall"
-        elif check == "Finished Track":
-            return newXPos, yPos, "Finished Track"
-        
-        return newXPos, newYPos, "Moved"
-        
-    def Act(self, oldYPos, oldXPos, oldYVel, oldXVel, accelX, accelY, deterministic=(False), crashType):
+    def driveTrack(self, oldYPos, oldXPos, oldYVel, oldXVel, accelX, accelY, deterministic, crashType):
         """
         This method generates the new state s' (position and velocity) from the old
         state s and the action a taken by the race car. It also takes as parameters
@@ -118,8 +91,184 @@ class ValueIteration:
                 self.track.restartRace()
             newVelX, newVelY = 0,0
         
-        return newY, newX, newVelX, newVelY
+        return newX, newY, newVelX, newVelY
 
 
+    def getNewPosition(self,xPos, yPos, xVel, yVel, punishment):
+        """
+        Get a new position using the old position and the velocity
+        Args:
+            xPos: The Old X position for the racer
+            yPos: The Old Y Position for the racer
+            xVel: The xVelocity of the racer
+            yVel: The y-Velocity of the racer
+            punishment: the crash penalty
+        Returns: 
+            The new X and Y coordinate and, but returns either the old position of 0,0 if racer crashed
+        """
+        newXPos= xPos+xVel
+        newYPos = yPos+yVel
+        check =self.track.racerPosition(newXPos, newYPos)
+        if check =="Hit Wall":
+            if punishment == "Restart":
+                newXPos = self.startLocations[0][0]
+                newYPos = self.startLocations[0][1]
+                return newXPos, newYPos, "Restart"
+            elif punishment == 'Resume':
+                newXPos = xPos
+                newYPos = yPos
+                return newXPos, newYPos, "Hit Wall"
+        elif check == "Finished Track":
+            return newXPos, yPos, "Finished Track"
         
+        return newXPos, newYPos, "Moved"
         
+    def getPolicyfromQ(self,cols, rows):
+        """
+        This method returns the policy pi(s) based on the action taken in each state
+        that maximizes the value of Q in the table Q[s,a]. This is pi*(s)...the
+        best action that the race car should take in each state is the one that 
+        maximizes the value of Q. (* means optimal)
+        Args: 
+            cols: Number of columns in the racetrack
+            rows: Number of rows in the racetrack 
+        Return: 
+            pi: the policy
+        """
+        pi={}
+
+        for x in range(rows):
+            for y in range(cols):
+                for velocityY in self.velocities: 
+                    for velocityX in self.velocities:
+                        pi[(x,y,velocityX,velocityY)]=self.actions[np.argmax(self.qTable[x][y][velocityY][velocityX])]
+    
+    def valueIter(self, crashType, reward, numTrainIter):
+        """
+        This method is the actual value iteration algorithm
+        Args: 
+            crashType: Defines what type of what happens when you crash 
+            reward: reward of the terminal states
+            numTrainIter: Number of training iterations. 
+        Return: 
+            policy pi(s) which maps a given state to an optimal action
+        """
+        #Calculate the number of rows and columns of the racetrack
+        rows = self.track.getXSize()
+        cols = self.track.getYsize()
+
+        #Generates the List comprehension for the entire file
+        #stored as Xposition, Yposition, VelocityY, VelocityX
+        values =[[[[random.Random() for rXVel in self.velocities] for rYvel in self.velocities]for rYPOs in (line)] for line in self.raceTrack]
+
+        # Set the finish line states to 
+        for x in range(rows):
+            for y in range(cols): 
+                if self.raceTrack[x][y]=='F':
+                    for vy in self.velocities:
+                        for vx in self.velocities:
+                            values[x][y][vy][vx]
+        
+        #Set finish line state-action pairs to 0
+        for x in range(rows):
+            for y in range(cols):
+                if self.raceTrack[x][y]=='F':
+                    for vy in self.velocities:
+                        for vx in self.velocities:
+                            for ai, a in enumerate(self.actions):
+                                self.qTable[x][y][vy][vx][ai]=reward
+        
+        # This is where we train the agent (i.e. race car). Training entails 
+        # optimizing the values in the tables of V(s) and Q(s,a)
+        for t in range(numTrainIter):
+ 
+            # Keep track of the old V(s) values so we know if we reach stopping 
+            # criterion
+            values_prev = deepcopy(values)
+ 
+            # When this value gets below the error threshold, we stop training.
+            # This is the maximum change of V(s)
+            delta = 0.0
+ 
+            # For all the possible states s in S
+            for x in range(rows):
+                for y in range(cols):
+                    for vy in self.velocities:
+                        for vx in self.velocities:
+                         
+                            # If the car crashes into a wall
+                            if self.raceTrack[x][y]=='#':
+ 
+                                # Wall states have a negative value
+                                # I set some arbitrary negative value since
+                                # we want to train the car not to hit walls
+                                values[x][y][vy][vx] = -9.9
+ 
+                                # Go back to the beginning
+                                # of this inner for loop so that we set
+                                # all the other wall states to a negative value
+                                continue
+ 
+                            # For each action a in the set of possible actions A
+                            for ai, a in enumerate(self.actions):
+ 
+                                # The reward is -1 for every state except
+                                # for the finish line states
+                                if self.raceTrack[x][y]=='F':
+                                    r = reward
+                                else:
+                                    r = -1
+ 
+                                # Get the new state s'. s' is based on the current 
+                                # state s and the current action a
+                                new_x, new_y, new_vx, new_vy = self.driveTrack(new_y, new_x, new_vy, new_vx, a[0], a[1], True, crashType)
+ 
+                                # V(s'): value of the new state when taking action
+                                # a from state s. This is the one step look ahead.
+                                value_of_new_state = values_prev[new_x][new_y][new_vy][new_vx]
+ 
+                                # Get the new state s'. s' is based on the current 
+                                # state s and the action (0,0)
+                                new_y, new_x, new_vy, new_vx = self.driveTrack(y,x,vy,vx,a[0],a[1],True,crashType)
+ 
+                                # V(s'): value of the new state when taking action
+                                # (0,0) from state s. This is the value if for some
+                                # reason the race car attemps to accelerate but 
+                                # fails
+                                value_of_new_state_if_action_fails = values_prev[new_x][new_y][new_vy][new_vx]
+ 
+                                # Expected value of the new state s'
+                                # Note that each state-action pair has a unique 
+                                # value for s'
+                                expected_value = (self.probAccelSuccess * value_of_new_state) + (self.probAccelFail * (value_of_new_state_if_action_fails))
+ 
+                                # Update the Q-value in Q[s,a]
+                                # immediate reward + discounted future value
+                                self.qTable[x][y][vy][vx][ai] = r + (self.discRate * expected_value)
+ 
+                            # Get the action with the highest Q value
+                            argMaxQ = np.argmax(self.qTable[x][y][vy][vx])
+ 
+                            # Update V(s)
+                            values[x][y][vy][vx] = self.qTable[x][y][vy][vx][argMaxQ]
+ 
+            # Make sure all the rewards to 0 in the terminal state
+            for x in range(rows):
+                for y in range(cols):
+                    # Terminal state has a value of 0
+                    if self.raceTrack[x][y]=='F':
+                        for vy in self.velocities:
+                            for vx in self.velocities:                 
+                                values[x][y][vy][vx] = reward
+ 
+            # See if the V(s) values are stabilizing
+            # Finds the maximum change of any of the states. Delta is a float.
+            delta = max([max([max([max([abs(values[x][y][vy][vx] - values_prev[x][y][vy][vx]) for vx in self.velocities]) for vy in (self.velocities)]) for y in range(cols)]) for x in range(rows)])
+ 
+            # If the values of each state are stabilizing, return the policy
+            # and exit this method.
+            if delta < self.errorThres:
+                return(self.getPolicyfromQ(cols,rows))
+        return(self.getPolicyfromQ(cols,rows))
+
+ 
