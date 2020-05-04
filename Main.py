3dac731f161154df5005d1d604c413df42b5de4a
nardielloam <nardielloam@gmail.com>
2020-05-04 14:29:34 -0600

    adds the final changes to the Value Iteration

--- Main.py
+++ Main.py
@@ -4,32 +4,32 @@ import ValueIteration
 def main():
     rTrack=raceTrack.RaceTrack()
     lTrack = raceTrack.RaceTrack()
-    rTrack.createTrack('C:/Users/nardi/Documents/2019-2020 School Year/Spring Semester/446/CSCI446Project4/R-track.txt')
-    lTrack.createTrack('C:/Users/nardi/Documents/2019-2020 School Year/Spring Semester/446/CSCI446Project4/L-track.txt')
+    rTrack.createTrack('R-track.txt')
+    lTrack.createTrack('L-track.txt')
     print(rTrack.printTrack())
     rValue = ValueIteration.ValueIteration(rTrack)
     policy = rValue.valueIter('Resume', 1000,10000)
-    print(rValue.timeTrial(policy, 10000, 'Resume', False))
+    print(rValue.timeTrial(policy, 10000, 'Resume', False, 'R-Track'))
 
     print(lTrack.printTrack())
     lValue= ValueIteration.ValueIteration(lTrack)
     lPolicy = lValue.valueIter('Resume', 1000,10000)
-    print(lValue.timeTrial(lPolicy, 10000, 'Resume', False))
+    print(lValue.timeTrial(lPolicy, 10000, 'Resume', False, 'L-Track'))
 
     oTrack = raceTrack.RaceTrack()
-    oTrack.createTrack('C:/Users/nardi/Documents/2019-2020 School Year/Spring Semester/446/CSCI446Project4/O-track.txt')
+    oTrack.createTrack('O-track.txt')
     oValue = ValueIteration.ValueIteration(oTrack)
     oPolicy = oValue.valueIter('Resume', 1000, 10000)
-    print(oValue.timeTrial(oPolicy, 10000, 'Resume', False))
+    print(oValue.timeTrial(oPolicy, 10000, 'Resume', False, 'O-Track'))
 
     rrPolicy = rValue.valueIter('Restart', 1000, 10000)
-    print(rValue.timeTrial(rrPolicy, 10000, 'Restart', False))
+    print(rValue.timeTrial(rrPolicy, 10000, 'Restart', False, 'R-Track'))
 
     lrPolicy = lValue.valueIter('Restart', 1000, 10000)
-    print(lValue.timeTrial(lrPolicy, 10000, 'Restart', False))
+    print(lValue.timeTrial(lrPolicy, 10000, 'Restart', False, 'L-Track'))
 
     orPolicy = lValue.valueIter('Restart', 1000, 10000)
-    print(oValue.timeTrial(orPolicy, 10000, 'Restart', False))
+    print(oValue.timeTrial(orPolicy, 10000, 'Restart', False, 'O-Track'))
 
 
 main()
